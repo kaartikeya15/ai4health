1771dceecdf3c818e483df01fffd93da5d990a93
Kaartikeya Panjwani <52821246+kaartikeya15@users.noreply.github.com>
2024-07-14 23:58:50 -0400

    Add files via upload

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"}j@3[=/.wD0,`%`>bYx3i(ob*kE8=rN lFj]Gr`e#ZeN!<ek26B br?>c=,_e!~{";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3564;}
